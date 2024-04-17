d0546594d494e2f477fbaac812ac85dc4174d064
Francis Conroy <francis.conroy@switchdin.com>
2024-04-17 13:50:36 +1000

    Add the basic webserver

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 A simple example that connects to the Adafruit IO MQTT server
 and publishes values that represent a sine wave
 """
-
+import socket
 import time
-from math import sin
 
-import network
 import machine
-from umqtt.simple import MQTTClient
+import network
 
 
 class OnboardLED:
@@ -39,10 +37,12 @@ class OnboardLED:
         self.blink_start()
 
     def blink_start(self, freq):
-        self.blink_timer.init(freq=freq, mode=machine.Timer.PERIODIC, callback=self.blink_toggle)
+        self.blink_timer.init(freq=freq, mode=machine.Timer.PERIODIC,
+                              callback=self.blink_toggle)
 
     def blink_stop(self):
         self.blink_timer.deinit()
+
 
 def connect_wifi():
     # Fill in your WiFi network name (ssid) and password here:
@@ -57,51 +57,55 @@ def connect_wifi():
         print('Waiting for connection...')
         time.sleep(1)
     print("Connected to WiFi")
+    print(wlan.ifconfig())
+
+def run_webserver():
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>Pico W</title> </head>
+        <body> <h1>Pico W</h1>
+            <p>%s</p>
+        </body>
+    </html>
+    """
+
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
+    # Listen for connections
+    while True:
+        try:
+            cl, addr = s.accept()
+            print('client connected from', addr)
+            request = cl.recv(1024)
+            print(request)
+
+            request = str(request)
+
+            response = html
+
+            cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+            cl.send(response)
+            cl.close()
+
+        except OSError as e:
+            cl.close()
+            print('connection closed')
+
+
 
 led = OnboardLED()
 led.blink_start(2)
 connect_wifi()
 led.blink_stop()
 led.on()
+run_webserver()
 
 
 
-# Fill in your Adafruit IO Authentication and Feed MQTT Topic details
-mqtt_host = "io.adafruit.com"
-mqtt_username = ""  # Your Adafruit IO username
-mqtt_password = ""  # Adafruit IO Key
-mqtt_publish_topic = ""  # The MQTT topic for your Adafruit IO Feed
 
-# Enter a random ID for this MQTT Client
-# It needs to be globally unique across all of Adafruit IO.
-mqtt_client_id = "somethingreallyrandomandunique123"
-
-# Initialize our MQTTClient and connect to the MQTT server
-mqtt_client = MQTTClient(
-    client_id=mqtt_client_id,
-    server=mqtt_host,
-    user=mqtt_username,
-    password=mqtt_password)
-
-mqtt_client.connect()
-
-# Publish a data point to the Adafruit IO MQTT server every 3 seconds
-# Note: Adafruit IO has rate limits in place, every 3 seconds is frequent
-#  enough to see data in realtime without exceeding the rate limit.
-counter = 0
-try:
-    while True:
-        # Generate some dummy data that changes every loop
-        sine = sin(counter)
-        counter += .8
-
-        # Publish the data to the topic!
-        print(f'Publish {sine:.2f}')
-        mqtt_client.publish(mqtt_publish_topic, str(sine))
-
-        # Delay a bit to avoid hitting the rate limit
-        time.sleep(3)
-except Exception as e:
-    print(f'Failed to publish message: {e}')
-finally:
-    mqtt_client.disconnect()
