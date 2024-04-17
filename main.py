674b47e7962d3a9f98ff14c741ceae2a05bc7a80
Francis Conroy <francis.conroy@switchdin.com>
2024-04-17 22:09:04 +1000

    Add basic I2c interaction

--- main.py
+++ main.py
@@ -104,8 +104,22 @@ led.blink_start(2)
 connect_wifi()
 led.blink_stop()
 led.on()
+mid = machine.unique_id()
+for i in mid:
+    print(hex(i))
+
+i2c = machine.I2C(0, freq=50000)  # create I2C peripheral at frequency of 400kHz
+# depending on the port, extra parameters may be required
+# to select the peripheral and/or pins to use
+
+for item in i2c.scan():  # scan for peripherals, returning a list of 7-bit addresses
+    print(item)
 run_webserver()
 
-
-
-
+# i2c.writeto(42, b'123')         # write 3 bytes to peripheral with 7-bit address 42
+# i2c.readfrom(42, 4)             # read 4 bytes from peripheral with 7-bit address 42
+#
+# i2c.readfrom_mem(42, 8, 3)      # read 3 bytes from memory of peripheral 42,
+#                                 #   starting at memory-address 8 in the peripheral
+# i2c.writeto_mem(42, 2, b'\x10') # write 1 byte to memory of peripheral 42
+#                                 #   starting at address 2 in the peripheral
